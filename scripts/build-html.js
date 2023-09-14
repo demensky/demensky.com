0ab4ca3716ed8f23d3c05f32f10983e1a1f9f997
demensky <dmitry@demensky.com>
2023-09-15 02:59:12 +0300

    docs: custom html

---
+++ scripts/build-html.js
@@ -0,0 +1,16 @@
+import {readFile, writeFile} from 'node:fs/promises';
+import minifyHtml from '@minify-html/node';
+
+let original = await readFile('src/index.html');
+
+const minified = minifyHtml.minify(original, {
+  do_not_minify_doctype: true,
+  ensure_spec_compliant_unquoted_attribute_values: true,
+  keep_spaces_between_attributes: true,
+  minify_css: true,
+  minify_css_level_1: true,
+  minify_css_level_2: true,
+  minify_css_level_3: true,
+});
+
+await writeFile('dist/index.html', minified);
---
+++ scripts/build-photo.js
@@ -0,0 +1,34 @@
+import sharp from 'sharp';
+
+const original = new sharp('src/photo.jpg');
+
+const scales = [1, 1.5, 2, 2.5, 3, 3.5, 4];
+const width = 192;
+const quality = 90;
+
+const promises = [];
+
+for (const scale of scales) {
+  const resize = original.clone().resize(width * scale);
+
+  promises.push(
+    resize
+      .clone()
+      .avif({quality, effort: 9})
+      .toFile(`dist/photo-${scale.toFixed(1)}.avif`),
+  );
+  promises.push(
+    resize
+      .clone()
+      .webp({quality, effort: 6})
+      .toFile(`dist/photo-${scale.toFixed(1)}.webp`),
+  );
+  promises.push(
+    resize
+      .clone()
+      .jpeg({quality, mozjpeg: true})
+      .toFile(`dist/photo-${scale.toFixed(1)}.jpg`),
+  );
+}
+
+await Promise.all(promises);
